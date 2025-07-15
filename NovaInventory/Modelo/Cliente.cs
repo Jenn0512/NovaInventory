537d4b19c093ee5c58fd941163f7643a0fa19b16
Jenn0512 <51299472+Jenn0512@users.noreply.github.com>
2025-07-15 11:40:13 -0600

    Agregar archivos de proyecto.

---
+++ NovaInventory/Modelo/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace NovaInventory.Modelo
+{
+    public class Cliente 
+
+    {
+        public int IdCliente { get; set; }
+        public String Nombre { get; set; }
+        public int IdEsCli {  get; set; }
+
+        public EstadoCliente EstadoCliente { get; set; }    
+    }
+}
---
+++ NovaInventory/Modelo/Producto.cs
@@ -0,0 +1,15 @@
+﻿namespace NovaInventory.Modelo
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string NombreProdcuto { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int IdEsPro {  get; set; }
+
+        public EstadoProducto EstadoProducto { get; set; }
+
+    }
+}
